437c7e74e5e529e8d5916b255cf3cfba3d35fd89
Ankit singh parihar <ankitsinghparihar@Ankits-MacBook-Air.local>
2025-07-30 18:21:10 +0530

    minor fix

--- frontend/src/api/floorPlanApi.js
+++ frontend/src/api/floorPlanApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api/floor-plan";
+const API_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8080/api/floor-plan";
 
 export const getFloorPlans = (username, version) =>
   axios.get(`${API_URL}?username=${username}&version=${version}`);
