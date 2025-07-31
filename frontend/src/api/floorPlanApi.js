cdbec9a223de3629cb4c67db840812af6b038f4c
Ankit singh parihar <ankitsinghparihar@Ankits-MacBook-Air.local>
2025-07-31 22:20:01 +0530

    revert

--- frontend/src/api/floorPlanApi.js
+++ frontend/src/api/floorPlanApi.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-const API_URL = import.meta.env.VITE_BACKEND_URL 
-  ? `${import.meta.env.VITE_BACKEND_URL}/api/floor-plan`
-  : "http://localhost:8080/api/floor-plan";
-  
+const API_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8080/api/floor-plan";
+
 export const getFloorPlans = (username, version) =>
   axios.get(`${API_URL}?username=${username}&version=${version}`);
 
